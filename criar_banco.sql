4409833f525349d3982323479faf11bd388b5bbf
Francisco Willian <113522254+WillianXG@users.noreply.github.com>
2024-05-09 20:51:33 -0300

    Update criar_banco.sql

--- criar_banco.sql
+++ criar_banco.sql
@@ -2,15 +2,12 @@ CREATE SCHEMA IF NOT EXISTS resilia;
 
 SET search_path TO resilia;
 
-CREATE TABLE Estudante (
+CREATE TABLE Curso (
     ID SERIAL PRIMARY KEY,
     nome VARCHAR(100),
-    idade INT,
-    email VARCHAR(100),
-    telefone VARCHAR(20),
-    status_evasao BOOLEAN,
-    id_turma INT,
-    FOREIGN KEY (id_turma) REFERENCES Turma(ID)
+    descricao TEXT,
+    duracao_meses INT,
+    qtd_modulo INT
 );
 
 CREATE TABLE Facilitador (
@@ -31,6 +28,29 @@ CREATE TABLE Turma (
     FOREIGN KEY (id_facilitador) REFERENCES Facilitador(ID)
 );
 
+CREATE TABLE Estudante (
+    ID SERIAL PRIMARY KEY,
+    nome VARCHAR(100),
+    idade INT,
+    email VARCHAR(100),
+    telefone VARCHAR(20),
+    status_evasao BOOLEAN,
+    id_turma INT,
+    FOREIGN KEY (id_turma) REFERENCES Turma(ID)
+);
+
+
+
+CREATE TABLE Especializacao (
+    ID_especializao serial primary key,
+    nome_especializao varchar(200),
+    id_facilitador int,
+    FOREIGN KEY (id_facilitador) REFERENCES Facilitador(ID)
+    
+);
+
+
+
 CREATE TABLE Modulo (
     ID SERIAL PRIMARY KEY,
     nome VARCHAR(100),
@@ -39,26 +59,4 @@ CREATE TABLE Modulo (
     FOREIGN KEY (id_curso) REFERENCES Curso(ID)
 );
 
-CREATE TABLE Curso (
-    ID SERIAL PRIMARY KEY,
-    nome VARCHAR(100),
-    descricao TEXT
-);
 
-
-
---exemplos para inserção
-INSERT INTO Estudante (nome, idade, email, telefone, status_evasao, id_turma)
-VALUES ('João', 20, 'joao@email.com', '123456789', FALSE, 1);
-
-INSERT INTO Facilitador (nome, especializacao, email, telefone)
-VALUES ('Maria', 'Matemática', 'maria@email.com', '987654321');
-
-INSERT INTO Curso (nome, descricao)
-VALUES ('Matemática Básica', 'Curso introdutório de matemática.');
-
-INSERT INTO Turma (nome, descricao, id_curso, id_facilitador)
-VALUES ('Turma A', 'Turma de matemática básica', 1, 1);
-
-INSERT INTO Modulo (nome, descricao, id_curso)
-VALUES ('Introdução à Aritmética', 'Números inteiros, operações básicas, etc.', 1);
